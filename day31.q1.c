b7e206f74595656c15b4ba731c0bd86ff4be0b2c
mittalridhima0806-tech <mittalridhima0806@gmail.com>
2026-03-03 12:27:03 +0530

    Create day31.q1.c
    Implement stack operations including push, pop, and display.

---
+++ day31.q1.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#define MAX 100
+
+int stack[MAX];
+int top = -1;
+
+// Push operation
+void push(int value) {
+    if (top == MAX - 1) {
+        printf("Stack Overflow\n");
+    } else {
+        top++;
+        stack[top] = value;
+    }
+}
+
+// Pop operation
+void pop() {
+    if (top == -1) {
+        printf("Stack Underflow\n");
+    } else {
+        printf("%d\n", stack[top]);
+        top--;
+    }
+}
+
+// Display operation
+void display() {
+    if (top == -1) {
+        printf("Stack is Empty\n");
+    } else {
+        for (int i = top; i >= 0; i--) {
+            printf("%d ", stack[i]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int n, choice, value;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &choice);
+
+        if (choice == 1) {
+            scanf("%d", &value);
+            push(value);
+        } 
+        else if (choice == 2) {
+            pop();
+        } 
+        else if (choice == 3) {
+            display();
+        }
+    }
+
+    return 0;
+}
