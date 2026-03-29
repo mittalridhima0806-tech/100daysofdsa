3eed06866663c3479438c3dd2dbfea761b6beb5a
mittalridhima0806-tech <mittalridhima0806@gmail.com>
2026-03-30 00:15:34 +0530

    Create day56.q1.c

---
+++ day56.q1.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define structure for tree node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create new node
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+// Function to check if two trees are mirror of each other
+int isMirror(struct Node* t1, struct Node* t2) {
+    if (t1 == NULL && t2 == NULL)
+        return 1;
+    if (t1 == NULL || t2 == NULL)
+        return 0;
+
+    return (t1->data == t2->data) &&
+           isMirror(t1->left, t2->right) &&
+           isMirror(t1->right, t2->left);
+}
+
+// Function to check if tree is symmetric
+int isSymmetric(struct Node* root) {
+    if (root == NULL)
+        return 1;
+    return isMirror(root->left, root->right);
+}
+
+// Build tree from level order input
+struct Node* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1)
+        return NULL;
+
+    struct Node* nodes[n];
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == -1)
+            nodes[i] = NULL;
+        else
+            nodes[i] = newNode(arr[i]);
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (nodes[i] != NULL) {
+            int leftIndex = 2 * i + 1;
+            int rightIndex = 2 * i + 2;
+
+            if (leftIndex < n)
+                nodes[i]->left = nodes[leftIndex];
+            if (rightIndex < n)
+                nodes[i]->right = nodes[rightIndex];
+        }
+    }
+
+    return nodes[0];
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    struct Node* root = buildTree(arr, n);
+
+    if (isSymmetric(root))
+        printf("YES\n");
+    else
+        printf("NO\n");
+
+    return 0;
+}
