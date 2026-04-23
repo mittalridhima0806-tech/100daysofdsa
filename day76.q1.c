54c898e8f18a8756f7ca98d9901f428747c355ff
mittalridhima0806-tech <mittalridhima0806@gmail.com>
2026-04-24 00:51:55 +0530

    Create day76.q1.c

---
+++ day76.q1.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+#define MAX 100
+
+int adj[MAX][MAX];
+int visited[MAX];
+
+// DFS function
+void dfs(int node, int n) {
+    visited[node] = 1;
+
+    for (int i = 1; i <= n; i++) {
+        if (adj[node][i] && !visited[i]) {
+            dfs(i, n);
+        }
+    }
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    // Initialize adjacency matrix
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
+            adj[i][j] = 0;
+
+    // Input edges
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        adj[u][v] = 1;
+        adj[v][u] = 1; // undirected
+    }
+
+    // Initialize visited
+    for (int i = 1; i <= n; i++)
+        visited[i] = 0;
+
+    int components = 0;
+
+    // Count components
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            dfs(i, n);
+            components++;
+        }
+    }
+
+    printf("%d\n", components);
+    return 0;
+}
